ffe49e12b0fc64dae2a62ddca4fac30da08f2ad3
Khoa Nguyen <ngdoan.anhkhoa@gmail.com>
2019-10-06 11:08:34 +0700

    Initial commit

---
+++ ClientApp/src/components/ExternalApi.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useAuth0 } from "../react-auth0-wrapper";
+
+const ExternalApi = () => {
+    const [showResult, setShowResult] = useState(false);
+    const [apiMessage, setApiMessage] = useState("");
+    const {
+        isAuthenticated,
+        logout,
+        loginWithRedirect,
+        getTokenSilently
+    } = useAuth0();
+    const logoutWithRedirect = () =>
+        logout({
+            returnTo: window.location.origin
+        });
+
+    const callApi = async () => {
+        try {
+            var token = "";
+            if (isAuthenticated)
+            {
+                token = await getTokenSilently();
+            }
+            const response = await fetch(
+                "https://localhost:5001/api/SampleData/WeatherForecasts",
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            const responseData = await response.json();
+
+            setShowResult(true);
+            setApiMessage(responseData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <>
+            <h1>External API</h1>
+            {!isAuthenticated && (
+                <button onClick={() => loginWithRedirect({})}>Login</button>
+            )}
+            {isAuthenticated && (
+                <button onClick={() => logoutWithRedirect()}>Logout</button>
+            )}
+            <button onClick={callApi}>Ping API</button>
+            {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
+        </>
+    );
+};
+
+export default ExternalApi;
---
+++ ClientApp/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExternalApi from './components/ExternalApi';
+import { Auth0Provider } from "./react-auth0-wrapper";
+import config from "./auth_config.json";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+    window.history.replaceState(
+        {},
+        document.title,
+        appState && appState.targetUrl ? appState.targetUrl : window.location.pathname
+    );
+};
+
+ReactDOM.render(
+    <Auth0Provider
+        domain={config.domain}
+        client_id={config.clientId}
+        redirect_uri={window.location.origin}
+        audience={config.audience}
+        onRedirectCallback={onRedirectCallback}
+    >
+        <ExternalApi />
+    </Auth0Provider>,
+    document.getElementById("root")
+);
